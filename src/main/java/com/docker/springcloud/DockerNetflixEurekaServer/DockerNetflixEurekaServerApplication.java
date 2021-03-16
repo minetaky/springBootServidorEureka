121db603a1fd4e6d3ab3547d87dfe71f984a05de
minetaky <minetaky@gmail.com>
2021-03-16 11:16:27 -0600

    Servidor Eureka para descubrimiento de Microservicios

---
+++ src/main/java/com/docker/springcloud/DockerNetflixEurekaServer/DockerNetflixEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.docker.springcloud.DockerNetflixEurekaServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DockerNetflixEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerNetflixEurekaServerApplication.class, args);
+	}
+
+}
